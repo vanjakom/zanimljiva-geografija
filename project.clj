61eb6a3fef40ecab126a15492c4a3ebc6e06b8a7
Vanja Komadinovic <vanjakom@gmail.com>
2026-01-14 13:05:12 +0100

    0.1.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject vanjakom/zanimljiva-geografija "0.1.0-SNAPSHOT"
+(defproject vanjakom/zanimljiva-geografija "0.1.0"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [
                  [org.clojure/clojure "1.11.1"]
-                 [com.mungolab/clj-common "0.3.0-SNAPSHOT"]
-                 [com.mungolab/clj-geo "0.2.0-SNAPSHOT"]])
+                 [com.mungolab/clj-common "0.3.3"]
+                 [com.mungolab/clj-geo "0.2.0"]])
